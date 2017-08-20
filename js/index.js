c06ffc6896fb24aacd8d5ea94958a3d676dda27b
sashaMJ <kurchavova.a@list.ru>
2017-08-20 14:24:11 +0300

    Js доработки

--- js/index.js
+++ js/index.js
@@ -3,14 +3,17 @@ $(function () {
     var mainHeader = $(".main-header");
     var menuTop = mainHeader.offset().top;
     var fixed = false;
+    var mainHeaderHelper = $(".main-header-helper");
 
     $(window)
         .on("scroll", function () {
             var winScrollTop = $(this).scrollTop();
             if (winScrollTop >= menuTop && !fixed) {
                 mainHeader.addClass("main-header-fixed");
+                mainHeaderHelper.addClass("show");
                 fixed = true;
             } else if (winScrollTop <= menuTop) {
+                mainHeaderHelper.removeClass("show");
                 mainHeader.removeClass("main-header-fixed");
                 fixed = false;
             }
